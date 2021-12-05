7cd27e2264b5caf7569120621d147a9a642ed100
Abdul Khalaque <limonbabu165@gmail.com>
2021-12-05 23:43:38 +0600

    btn2

---
+++ main.js
@@ -0,0 +1,6 @@
+function myFunc(){
+    var x = document.getElementById("cap1");
+    x.value = x.value.toUpperCase();
+    var y = document.getElementById("cap2");
+    y.value = y.value.toUpperCase();
+}
