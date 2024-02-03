29b93f0238b0c15c7972358cf5df52717ee35b90
Limon Islam <limon4u714@gmail.com>
2024-02-03 22:34:10 +0600

    Date filter

--- main.js
+++
@@ -1,6 +0,0 @@
-function myFunc(){
-    var x = document.getElementById("cap1");
-    x.value = x.value.toUpperCase();
-    var y = document.getElementById("cap2");
-    y.value = y.value.toUpperCase();
-}
